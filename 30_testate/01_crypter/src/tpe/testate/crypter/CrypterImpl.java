6866eb751b9603f4ffab9ac9e205b9b6058393a8
s.abdi <s.abdi@bw-lp>
2016-12-19 15:51:28 +0100

    issues solved

--- 30_testate/01_crypter/src/tpe/testate/crypter/CrypterImpl.java
+++ 30_testate/01_crypter/src/tpe/testate/crypter/CrypterImpl.java
@@ -1,53 +1,53 @@
 package tpe.testate.crypter;
 
 /**
- * Klasse, die eine eingegebene Zeichenkette mithilfe eines 
- * Codebaumes ver-/entschlüsselt.
- * 
+ * Klasse, die eine eingegebene Zeichenkette mithilfe eines
+ * Codebaumes ver-/entschlï¿½sselt.
+ *
  * @author 1615141
  */
 public class CrypterImpl implements Crypter {
 
 
 	/**
-	 * Eingegebener Text wird verschlüsselt.
-	 * 
-	 * @param input Text der verschlüsselt werden soll.
-	 * @return verschlüsselter Text.
+	 * Eingegebener Text wird verschlï¿½sselt.
+	 *
+	 * @param input Text der verschlï¿½sselt werden soll.
+	 * @return verschlï¿½sselter Text.
 	 */
 	@Override
 	public String encrypt(String input) {
 		String cryptedText = "";
 		char[] array = (input.toLowerCase()).toCharArray();
 		for (int i = 0; i < array.length; i++) {
-			if ((array[i] >= '0' && array[i] <= '9') || 
-			    (array[i] >= 'a' && array[i] <= 'z') || 
-			     array[i] == ' ') {
+			 if ((array[i] >= '0' && array[i] <= '9') ||
+				 (array[i] >= 'a' && array[i] <= 'z') ||
+				  array[i] == ' ') {
 				switch(array[i]){
-				case 'e': { cryptedText=cryptedText+'3'; break;}
-				case '3': { cryptedText=cryptedText+'3'; break;}
-				case 'l': { cryptedText=cryptedText+'l'; break;}
-				case '1': { cryptedText=cryptedText+'1'; break;}
-				case 'o': { cryptedText=cryptedText+'0'; break;}
-				case '0': { cryptedText=cryptedText+'o'; break;}
-				case 'a': { cryptedText=cryptedText+'4'; break;}
-				case '4': { cryptedText=cryptedText+'a'; break;}
-				case 't': { cryptedText=cryptedText+'7'; break;}
-				case '7': { cryptedText=cryptedText+'t'; break;}
-				default :  { cryptedText= cryptedText+array[i]; break;}
+				case 'e':  cryptedText=cryptedText+'3'; break;
+				case '3':  cryptedText=cryptedText+'e'; break;
+				case 'l':  cryptedText=cryptedText+'1'; break;
+				case '1':  cryptedText=cryptedText+'l'; break;
+				case 'o':  cryptedText=cryptedText+'0'; break;
+				case '0':  cryptedText=cryptedText+'o'; break;
+				case 'a':  cryptedText=cryptedText+'4'; break;
+				case '4':  cryptedText=cryptedText+'a'; break;
+				case 't':  cryptedText=cryptedText+'7'; break;
+				case '7':  cryptedText=cryptedText+'t'; break;
+				default :  cryptedText=cryptedText+array[i]; break;
 				}
 			}
 		}
 		return cryptedText;
 	}
-	
+
 	/**
-	 * Eingegebener Text wird verschlüsselt.
-	 * 
-	 * @param input Text der entschlüsselt werden soll.
-	 * @return entschlüsselter Text.
+	 * Eingegebener Text wird verschlï¿½sselt.
+	 *
+	 * @param input Text der entschlï¿½sselt werden soll.
+	 * @return entschlï¿½sselter Text.
 	 * @throws IllegalArgumentException wird geworfen, wenn die Eingabe
-     *         ungültige Zeichen (Großbuchstaben) enthält.
+     *         ungï¿½ltige Zeichen (Groï¿½buchstaben) enthï¿½lt.
 	 */
 	@Override
 	 public String decrypt(String input) throws IllegalArgumentException {
@@ -55,13 +55,13 @@ public class CrypterImpl implements Crypter {
 		      if (Character.isUpperCase(input.charAt(i))) {
 		        throw new IllegalArgumentException();
 		      }
-		      if (!((input.charAt(i) >= 'a' && input.charAt(i) <= 'z') 
-		          || input.charAt(i) >= '0' && input.charAt(i) <= '9' 
+		      if (!((input.charAt(i) >= '0' && input.charAt(i) <= '9')
+		          || input.charAt(i) >= 'a' && input.charAt(i) <= 'z'
 		          || input.charAt(i) == ' ')) {
 		        throw new IllegalArgumentException();
 		      }
 		    }
 		    return encrypt(input);
 		}
-		
+
 }
